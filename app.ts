d5affa472ca7c0513987870d04b3ced0662307af
Frederik Hess <fred@hess-pedersen.dk>
2023-03-21 10:11:14 +0100

    implemented 404 error handling

--- app.ts
+++ app.ts
@@ -23,4 +23,12 @@ app.use('/api/v1/recipes', recipeRouter);
 app.use('/api/v1/shoppinglists', shoppinglistRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
+
+
 export default app;
